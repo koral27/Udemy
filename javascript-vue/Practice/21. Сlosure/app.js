f9c3fc60bc35ab54d71ff99483e63bb4383989b6
koral27 <38889539+koral27@users.noreply.github.com>
2020-02-09 19:16:10 +0300

    Create app.js

---
+++ javascript-vue/Practice/21. Сlosure/app.js
@@ -0,0 +1,32 @@
+// Создайте функцию которая бы умела делать:
+// minus(10)(6); // 4
+// minus(5)(6); // -1
+// minus(10)(); // 10
+// minus()(6); // -6
+// minus()(); // 0
+// Подсказка, функция minus должна возвращать другую функцию.
+
+let minus = function (a){
+    return function(b){
+        return a - b
+    }
+}
+
+let result = minus(10)(6);
+console.log(result);
+
+// Реализовать функцию, которая умножает и умеет запоминать возвращаемый результат между вызовами:
+// function multiplyMaker ...
+// const multiply = multiplyMaker(2);
+// multiply(2); // 4 (2 * 2)
+// multiply(1); // 4 (4 * 1)
+// multiply(3); // 12 (4 * 3)
+// multiply(10); // 120 (12 * 10)
+
+function multiplyMaker(a){
+    return function(b){
+        return a * b
+    }
+}
+const multiply = multiplyMaker(2);
+console.log( multiply(3) );
